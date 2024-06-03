a50625faaffcb4952af4474860ea7d08a5a6a3b4
unknown <you@example.com>
2024-06-03 17:32:32 +0300

    Project updated until views

--- marketplace/marketplaceapp/models.py
+++ marketplace/marketplaceapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+#coloane: name, desription, ingredients, price, weight_grams,
+# available_quantity, image
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    ingredients = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    weight = models.IntegerField()
+    available_quantity = models.IntegerField()
+    image = models.ImageField(upload_to="product_images", null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
--- marketplace/marketplaceapp/urls.py
+++ marketplace/marketplaceapp/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("products", views.product_list, name="product_list"),
+    path("products/<int:product_id>", views.product_detail, name="product_detail")
 ]
